d04592fef878927aa650d8fdc5a280659b33f99f
Brad Stokes <bradleymstokes@gmail.com>
2018-02-26 13:29:47 +1100

    Update thingy

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const ExtractTextPlugin  = require('extract-text-webpack-plugin');
-const CSSExtract = new ExtractTextPlugin('styles.css');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
-module.exports = (env, args) =>{
+module.exports = (env) => {
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: './src/app.js',
@@ -21,8 +20,18 @@ module.exports = (env, args) =>{
         test: /\.s?css$/,
         use: CSSExtract.extract({
           use: [
-            'css-loader',
-            'sass-loader'
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
           ]
         })
       }]
@@ -30,10 +39,10 @@ module.exports = (env, args) =>{
     plugins: [
       CSSExtract
     ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true
     }
   };
-} 
+};
